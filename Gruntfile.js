4deffd47a7b094e1a2205f7fd9978be3a2882d74
Phil Cable <pcable@wsu.edu>
2018-07-17 22:41:40 -0700

    Set up JavaScript linting

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,12 +10,44 @@ module.exports = function( grunt ) {
 				bin: "vendor/bin/phpcs --extensions=php --ignore=\"*/vendor/*,*/node_modules/*\"",
 				standard: "phpcs.ruleset.xml"
 			}
-		}
+		},
+
+		jscs: {
+			scripts: {
+				src: "js/*.js",
+				options: {
+					preset: "jquery",
+					maximumLineLength: 250
+				}
+			}
+		},
+
+		jshint: {
+			people_scripts: {
+				src: "js/*.js",
+				options: {
+					bitwise: true,
+					curly: true,
+					eqeqeq: true,
+					forin: true,
+					freeze: true,
+					noarg: true,
+					nonbsp: true,
+					quotmark: "double",
+					undef: true,
+					unused: true,
+					browser: true, // Define globals exposed by modern browsers.
+					jquery: true,  // Define globals exposed by jQuery.
+				}
+			}
+		},
 
 	} );
 
+	grunt.loadNpmTasks( "grunt-contrib-jshint" );
+	grunt.loadNpmTasks( "grunt-jscs" );
 	grunt.loadNpmTasks( "grunt-phpcs" );
 
 	// Default task(s).
-	grunt.registerTask( "default", [ "phpcs" ] );
+	grunt.registerTask( "default", [ "phpcs", "jscs", "jshint" ] );
 };
